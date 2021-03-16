88454d8cbde4d0539c0647b69c70792897cd6498
poperlo <alexdudakov@gmail.com>
2021-03-17 00:29:37 +0300

    Update Main.java
    Add scanner and EquationParser invocation

--- src/com/aleksey/java/calc/Main.java
+++ src/com/aleksey/java/calc/Main.java
@@ -1,7 +1,17 @@
 package com.aleksey.java.calc;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        String equation = scanner.nextLine();
+//        String equation = "5 + 6";
+
+        System.out.println(EquationParser.getCalculator(equation)
+                .calculate()
+                .getResult());
     }
 }
